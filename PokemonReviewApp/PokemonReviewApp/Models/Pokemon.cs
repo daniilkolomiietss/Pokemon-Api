1204ca3970b53abf6ab5c756123dc48109fdeeb6
Daniil Kolomiiets <daniilkolomii@gmail.com>
2026-07-13 15:46:42 +0300

    initial commit

---
+++ PokemonReviewApp/PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FavoriteFood { get; set; }
+        public bool IsWild { get; set; }
+        public int HappinessLevel
+        {
+            get; set
+            {
+                if (value < 0 && value > 10)
+                    throw new ArgumentOutOfRangeException(nameof(HappinessLevel), "Happiness level cannot be negative or bigger than 10");
+                field = value;
+            }
+        }
+    }
+}
